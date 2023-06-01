e011a8228cfaf75b67669b7d0f0c6f67eda885d5
Pushpanjali SK <pushpanjali.Kharatmol@gmail.com>
2023-06-01 20:18:00 +0530

    2. Java Stdin and Stdout I

---
+++ Java/JavaStdinANDStdoutI.java
@@ -0,0 +1,24 @@
+// Title    - Java Stdin and Stdout I
+// Link     - https://www.hackerrank.com/challenges/java-stdin-and-stdout-1/submissions/code/287347103
+// Solution -  as below
+
+import java.util.*;
+
+public class JavaStdinANDStdoutI {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        scan.close();
+        // Complete this line
+        
+        // Complete this line
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        // Complete this line
+        // Complete this line
+    }
+}
