4e907ff36aee1c0164b0bf3b19d6a742bfd0911e
Pushpanjali SK <pushpanjali.Kharatmol@gmail.com>
2023-06-01 20:15:36 +0530

    1. Welcome to Java - Program

---
+++ Java/WelcomeTOJava.java
@@ -0,0 +1,11 @@
+//  Title   - Welcome to Java
+//  Link    - https://www.hackerrank.com/challenges/welcome-to-java/submissions/code/287346530
+// Solution - as below 
+
+public class WelcomeTOJava {
+    public static void main(String[] args) {
+        /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
