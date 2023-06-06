786add99790f0749a37cf36d1c463defaa790d68
Pushpanjali SK <pushpanjali.Kharatmol@gmail.com>
2023-06-06 13:50:19 +0530

    5. Java Output Formatting

---
+++ Java/JavaOutputFormatting.java
@@ -0,0 +1,24 @@
+// Title    - 5. Java Output Formatting
+// Link     - https://www.hackerrank.com/challenges/java-output-formatting/submissions/code/287967512
+// Solution -  as below
+import java.util.Scanner;
+
+public class JavaOutputFormatting {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String lang[] = { "", "", "" };
+        int x[] = { 0, 0, 0 };
+        for (int i = 0; i < 3; i++) {
+            lang[i] = sc.next();
+            x[i] = sc.nextInt();
+            // Complete this line
+        }
+
+        System.out.println("================================");
+        for (int i = 0; i < 3; i++) {
+            System.out.printf("%-15s%03d\n", lang[i], x[i]);
+        }
+        System.out.println("================================");
+        sc.close();
+    }
+}
---
+++ Java/StdinANDStdoutI.java
@@ -0,0 +1,25 @@
+// Title    - 2. Java Stdin and Stdout I
+// Link     - https://www.hackerrank.com/challenges/java-stdin-and-stdout-1/submissions/code/287347103
+// Solution -  as below
+
+import java.util.*;
+
+public class StdinANDStdoutI {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        scan.close();
+        // Complete this line
+        
+        // Complete this line
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        // Complete this line
+        // Complete this line
+    }
+}
